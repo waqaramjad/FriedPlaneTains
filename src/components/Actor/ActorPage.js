ec8955b3d9ce5aaa12a343befcb634edb623139a
waqaramjad <waqaramjad345@gmail.com>
2019-02-19 12:51:08 +0500

    actors renderc

--- src/components/Actor/ActorPage.js
+++ src/components/Actor/ActorPage.js
@@ -11,18 +11,71 @@ class ActorPage extends Component {
   constructor(props) {
 
     super(props);
+    this.state = {
+      Actors : ''
+    }
     // this.props.AcrtorsData()
     console.log(this.props.AcrtorsData())
 console.log(this.props.Actors)
+var actors = this.props.Actors
+// this.setState({
+//   Actors : actors
+// })
   }
    componentDidMount() {
     // this.props.GetAllNewsFromFirebase();
   }
   render() {
+    console.log(this.props.Actors)
+    console.log('sd',this.state.Actors)
+    var that = this
+
     return (
 
 <div class="container">
       <div className="row">
+      {
+                  
+              
+
+                    
+                  this.props.Actors!=undefined ?   Object.keys(this.props.Actors).map((data, index) => {
+                  //    var todos = this.state.Sports['data']
+                  var myActors = that.props.Actors[data]
+
+                  console.log(myActors)
+                  return(
+                    <div className="col-lg-3 col-md-3  col-6">
+                    <Link to="/detail/interview" style={{color:'black',textDecoration:'none'}}> 
+                      <div className="card card-inverse card-info text-center">
+                        <img
+                          className="moviePosterImg image"
+                          src={myActors.imageURL}
+                          alt="Card image cap"
+                        />
+                        <div className="card-block">
+                          <h4 className="card-title text-center">{myActors.name}</h4>
+                          {/* <p className="text-center"> Year: 2016 </p> */}
+                          <p className="text-center">
+                            {" "}
+                            rating: <span class="fa fa-star checked" />
+                            <span class="fa fa-star checked" />
+                            <span class="fa fa-star checked" />
+                            <span class="fa fa-star  " />
+                            <span class="fa fa-star" />
+                          </p>
+                        </div>
+                      </div>
+                      </Link>
+                    </div>
+
+                  )
+                  // console.log(index)
+                  // SportsArr.push(data)
+// console.log(SportsArr)
+                  })  :null
+
+}
         <div className="col-lg-3 col-md-3 col-6 ">
         <Link to="/detail/Antman" style={{color:'black',textDecoration:'none'}}> 
           <div className="card">
@@ -318,8 +371,9 @@ console.log(this.props.Actors)
 }
 
 function mapStateToProp(state) {
+  console.log(state.reducer)
   return {
-    Actors: state.reducer.Actors,
+    Actors: state.reducer.ACTORS,
     // CurrentUser: state.reducer.currentUser,AllNews: state.reducer.News
   };
 }
