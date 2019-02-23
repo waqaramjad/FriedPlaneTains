aba916c4ed138e62196bf58334d00bfcc7ed8476
waqaramjad <waqaramjad345@gmail.com>
2019-02-23 11:38:52 +0500

    removed

--- src/store/actions/action.js
+++ src/store/actions/action.js
@@ -28,19 +28,6 @@ console.log(data)
     return { type: 'ALL_USER', payload: allUsers }
 
 }
-// export function MOVIESData() {
-//   return dispatch => {
-//   var data = 'ifs'
-//     firebase.database().ref('/movies').once('value')
-//     .then((data) => {
-
-
-//     })
-
-//     return { type: 'MOVIES', payload: data }
-
-// }
-// }
 
 export function GetCommentsofMovie(data){
   let arrayofComments=[];
@@ -188,27 +175,6 @@ export function AcrtorsData() {
   }
 }
 
-// export function AcrtorsData() {
-//   return dispatch => {
-//       console.log('AcrtorsData')
-
-//       firebase.database().ref('Actors/').once('value')
-//       .then((data) => {
-
-//               console.log('Actors')
-//               console.log(data.val())
-//               let Actors = data.val();
-
-//               dispatch({ type: 'ACTORS', payload: Actors })
-
-
-
-
-
-//           })
-//   }
-// }
-
 export function CommentOnNews(data) {
   return dispatch => {
       console.log('commenting on news ')
@@ -324,122 +290,6 @@ export function getSaveList() {
   }
 }
 }
-// export function SignUpFirebase(load) {
-//     // firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user) {
-//     //     return user.updateProfile({'displayName : document.getElementById("name").value'})}).catch(function(error) {
-//     //     console.log(error);
-//     //   });
-//     // console.log(emailuser);
-//     // console.log(passworduser);
-//     return dispatch => {
-//         firebase.auth().createUserWithEmailAndPassword(load.email, load.pass)
-//             .then(users => {
-//                 //var user = firebase.auth().currentUser;
-
-//                 // console.log(username);
-//                 //let name=this.state.name;
-//                 users.updateProfile({
-//                     displayName: load.username
-//                 }).then(() => {
-//                     // Update successful.
-//                     console.log("updated successfully");
-//                     // this.onSuccess();
-//                     //
-//                     console.log(users);
-//                     let obj = {
-//                         displayName: users.displayName,
-//                         email: users.email,
-//                         uid: users.uid
-//                     }
-//                     firebase.database().ref("/users/" + obj.uid + "/").set(obj).then(() => {
-//                         console.log("user saved!");
-//                         dispatch(getUser());
-//                         History.push('/signin');
-//                     })
-
-//                 }).catch(function (error) {
-//                     // An error happened.
-//                     //   console.log(error);
-//                 });
-//             }).catch(function (error) {
-//                 // Handle Errors here.
-//                 var errorCode = error.code;
-//                 var errorMessage = error.message;
-//                 console.log(errorCode);
-//                 console.log(errorMessage);
-//                 // ...
-//             });
-//     }
-// }
-
-
-
-// function getCurrentUser(obj) {
-//     let CurrentUser = {
-//         displayName: obj.displayName,
-//         email: obj.email,
-//         uid: obj.uid
-//     }
-//     return {
-//         type: 'CURRENT_USER',
-//         payload: CurrentUser
-//     }
-// }
-// export function SignInFirebase(obj) {
-//     return dispatch => {
-//         firebase.auth().signInWithEmailAndPassword(obj.email, obj.password).then((user) => {
-//             console.log(user);
-//             dispatch(getCurrentUser(user));
-//             dispatch(getUser());
-//             // .then(()=>{
-//             // History.push('/homepage')
-//             // })
-
-//         })
-//     }
-// }
-
-// export function getAllMsg() {
-//     return dispatch => {
-
-//         return firebase.database().ref('/msg').on('value', snapshot => {
-//             const arr = [];
-//             console.log(snapshot.val());
-//             arr.push(snapshot.val());
-//             console.log('getallmsg');
-//             snapshot.forEach(obj=>{
-//                arr.push( obj.val());
-//             })
-//             // dispatch({ type: 'ALL_MSG', payload: arr })
-//             console.log('endo of allmsg')
-//             dispatch({ type: 'ALL_MSG', payload: arr });
-//         })
-
-//     }
-// }
-// export function getUpdatedMsg() {
-//     return dispatch => {
-
-//         // let arr = [];
-//         // firebase.database().ref('/msg').on('child_added', snapshot => {
-//         //     console.log(snapshot.val());
-//         //     arr.push(snapshot.val());
-//         //     console.log('updated msg :D');
-//         //     dispatch({ type: 'ALL_MSG', payload: arr });
-//         // })
-
-//     }
-// }
-
-// export function SendMessage(obj) {
-//     return dispatch => {
-//         obj.created = firebase.database.ServerValue.TIMESTAMP;
-//         obj.seen = false;
-//         console.log(obj);
-//         firebase.database().ref('/msg/').push(obj).then((a) => { console.log('done', a) })
-//         // dispatch(getAllMsg());
-//     }
-// }
 export function GetAllNews() {
   let arrayofNews = [];
   return dispatch => {
